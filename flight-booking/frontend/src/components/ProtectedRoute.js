3ff2b8ff516bc93483c96bcf23608838161e6fc5
pakanati vishnuvardhan reddy <153382490+Vishnu5762@users.noreply.github.com>
2025-11-03 20:36:57 +0530

    Update ProtectedRoute.js

--- flight-booking/frontend/src/components/ProtectedRoute.js
+++ flight-booking/frontend/src/components/ProtectedRoute.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ user, children }) => {
+const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext); 
+
+    if (loading) {
+        // 🟢 2. Show a placeholder while checking localStorage
+        return <div>Loading user session...</div>; 
+    }
   if (!user) {
     return <Navigate to="/login" replace />;
   }
