30e1d609486e819aadfc16ee82e1cb8024c99ca7
pakanati vishnuvardhan reddy <153382490+Vishnu5762@users.noreply.github.com>
2025-11-03 16:31:49 +0530

    Update server.js

--- flight-booking/backend/server.js
+++ flight-booking/backend/server.js
@@ -9,7 +9,10 @@ import bookingRoutes from './routes/bookingRoutes.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: 'https://flight-booking-frontend-yapp.onrender.com', 
+    credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
